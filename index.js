18755eb367c95eb202ffb44f14459ab084c5d564
Anirup Patnaik <anirup049@gmail.com>
2021-07-04 18:09:08 +0530

    Data Abstraction Implemented

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ newCircle.draw();
 
 function Circle(radius) {
   this.radius = radius;
+  this.computeOptimalLocation = function (factor) {
+    //...
+  };
   this.draw = function () {
+    this.computeOptimalLocation(0.1);
     console.log("in constructor function");
   };
 }
@@ -70,3 +74,35 @@ console.log(keys);
 if ("radius" in anotherCircle) {
   console.log("Radius property exists");
 }
+
+//-----------------------------------------
+
+// Data Abstraction
+
+// we are able to access this method of the Object..
+anotherCircle.computeOptimalLocation();
+
+// hence we should implement the Data abstraction concept here...
+// to show the essentials and hide the rest.
+
+function Car(width, height) {
+  this.width = width;
+  this.height = height;
+
+  forward = () => {
+    console.log("move forward!");
+  };
+
+  backward = () => {
+    console.log("Move Backword!");
+  };
+
+  this.drive = () => {
+    backward();
+    console.log("Lets drive now!");
+    forward();
+  };
+}
+
+let sedan = new Car(40, 50);
+sedan.drive();
